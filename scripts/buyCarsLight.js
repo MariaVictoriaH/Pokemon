eb3e9d9e0cca93c93fcc433b33746d35600ce212
Maria Victoria <mariahb7@outlook.com>
2023-06-11 15:34:49 -0500

    se ajusta diseño en tabs y cre da estilos al botón buy

--- scripts/buyCarsLight.js
+++ scripts/buyCarsLight.js
@@ -83,6 +83,7 @@ function createPokemon(pokemon) {
   pokeName.classList.add("name");
   i.classList.add("heart");
   img.classList.add("fondoImagen");
+  button.classList.add('btnBuy');
   img.src = pokemon.sprites.other["official-artwork"].front_shiny,
   powerLevel.textContent = `Power level:  ${pokemon.base_experience}`;
   button.textContent = "Buy";
@@ -117,6 +118,110 @@ function createPokemon(pokemon) {
 
 
 
+
+
+
+
+
+
+/* Llamo una única vez a la Api y asigno el resultado a un arreglo 
+function loadPokemonesApi()
+{
+  fetch(urlApi)
+    .then((res) => res.json())
+    .then((data) => {
+      //pokemones.push(data);
+      data.results.forEach((pokemon) => {
+        console.log(pokemon);
+        const div = document.createElement("div");
+        const pokeName = document.createElement("p");
+        const img = document.createElement("img");
+        const powerLevel = document.createElement("p");
+        const button = document.createElement("button");
+        const divCardHeader = document.createElement("div");
+        const divCardFooter = document.createElement("div");
+        const i = document.createElement("i");
+      
+        div.classList.add("item");
+        pokeName.textContent = `Name:  ${pokemon.name}`;
+        pokeName.classList.add("name");
+        i.classList.add("heart");
+        img.classList.add("fondoImagen");
+        img.src = pokemon.sprites.other["official-artwork"].front_shiny,
+        powerLevel.textContent = `Power level:  ${pokemon.base_experience}`;
+        button.textContent = "Buy";
+        listPokemon.appendChild(div);
+        divCardHeader.appendChild(pokeName);
+        divCardHeader.appendChild(i);
+        div.appendChild(divCardHeader);
+      
+        div.appendChild(img);
+        divCardFooter.appendChild(powerLevel);
+        divCardFooter.appendChild(button);
+        div.appendChild(divCardFooter);
+        
+      });
+
+
+      
+    });
+}
+
+function createPokemon(pokemon) {
+  console.log(pokemon);
+  const div = document.createElement("div");
+  const pokeName = document.createElement("p");
+  const img = document.createElement("img");
+  const powerLevel = document.createElement("p");
+  const button = document.createElement("button");
+  const divCardHeader = document.createElement("div");
+  const divCardFooter = document.createElement("div");
+  const i = document.createElement("i");
+
+  div.classList.add("item");
+  pokeName.textContent = `Name:  ${pokemon.name}`;
+  pokeName.classList.add("name");
+  i.classList.add("heart");
+  img.classList.add("fondoImagen");
+ // img.src = pokemon.sprites.other["official-artwork"].front_shiny,
+  powerLevel.textContent = `Power level:  ${pokemon.base_experience}`;
+  button.textContent = "Buy";
+  listPokemon.appendChild(div);
+  divCardHeader.appendChild(pokeName);
+  divCardHeader.appendChild(i);
+  div.appendChild(divCardHeader);
+
+  div.appendChild(img);
+  divCardFooter.appendChild(powerLevel);
+  divCardFooter.appendChild(button);
+  div.appendChild(divCardFooter);
+}
+
+function selectTab(evt, tabName) {
+  let i, tabcontent, tablinks, line;
+  line = document.querySelector('.line')
+  line.style.width = evt.target.offSetWidth + 'px';
+  line.style.left = evt.target.offSetLeft + 'px';
+  if (selectTab =='All'){
+    fetchPokemons(offSet, loadCards);
+  }
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("active", "");
+  }
+  
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += "active";
+}
+
+
+  loadPokemonesApi();
+
+  */
 
 
 
